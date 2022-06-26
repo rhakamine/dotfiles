6accb58de8cbbc150ac9f76e248e28652ab679f3
Rodrigo Akamine <rodrigo.heira.akamine@gmail.com>
2022-06-26 11:22:02 -0300

    Removed unused scripts

--- scripts/vpn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-tmux new -d -s vpn
-tmux send 'sudo openvpn --config ~/Documents/work/credenciais/operador25.ovpn --auth-retry interact --auth-user-pass ~/Documents/work/credenciais/login_op25.conf' ENTER
-tmux a -t vpn
